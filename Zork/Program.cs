38913209bf05a1bf0469af3f8e8acfeaf34b50ab
Deedo67 <70287389+Deedo67@users.noreply.github.com>
2021-09-26 18:44:42 -0400

    Zork 3.1 Commit
    Complete and Final commit for Zork part 3.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -4,7 +4,7 @@ namespace Zork
 {
     class Program
     {
-        private static string CurrentRoom
+        private static Room CurrentRoom
         {
             get
             {
@@ -15,6 +15,7 @@ namespace Zork
         static void Main(string[] args)
         {
             bool result = true | Move(Commands.UNKNOWN);
+            InitializeRoomDescriptions();
 
             Console.WriteLine("Welcome to Zork!");
 
@@ -27,32 +28,28 @@ namespace Zork
                     break;
                 }
 
-                string outputString;
                 switch (command)
                 {
                     case Commands.QUIT:
-                        outputString = "Thank you for playing!";
+                        Console.WriteLine("Thank you for playing!");
                         break;
 
                     case Commands.LOOK:
-                        outputString = "This is an open field west of a white house, with a boarded front door.\nA rubber mat saying 'Welcome to Zork!' lies by the door.";
+                        Console.WriteLine(CurrentRoom.Description);
                         break;
 
                     case Commands.NORTH:
                     case Commands.SOUTH:
                     case Commands.EAST:
                     case Commands.WEST:
-                        outputString = Move(command) ? $"You moved {command}." : "The way is shut!";
+                        Console.WriteLine(Move(command) ? $"You moved {command}." : "The way is shut!");
                         break;
 
                     default:
-                        outputString = "Unknown command.";
+                        Console.WriteLine("Unknown command.");
                         break;
                 }
-
-                Console.WriteLine(outputString);
             }
-
             Console.WriteLine("Finished.");
         }
 
@@ -89,13 +86,28 @@ namespace Zork
 
         private static Commands ToCommand(string commandString) => Enum.TryParse(commandString, true, out Commands result) ? result : Commands.UNKNOWN;
 
-        private static readonly string[,] Rooms =
+        private static readonly Room[,] Rooms =
         {
-            { "Rocky Trail", "South of House", "Canyon View" },
-            { "Forest", "West of House", "Behind House" },
-            { "Dense Woods", "North of House", "Clearing" }
+            { new Room("Rocky Trail"), new Room("South of House"), new Room("Canyon View") },
+            { new Room("Forest"), new Room("West of House"), new Room("Behind House") },
+            { new Room ("Dense Woods"), new Room("North of House"), new Room("Clearing") }
         };
 
         private static (int Row, int Column) Location = (1, 1);
+
+        private static void InitializeRoomDescriptions()
+        {
+            Rooms[0, 0].Description = "You are on a rock-strewn trail.";
+            Rooms[0, 1].Description = "You are facing the south side of a white house. There s no door here and all the windows are barred.";
+            Rooms[0, 2].Description = "You are at the top of the Great Conyon on its south wall.";
+
+            Rooms[1, 0].Description = "This is a forest, with trees in all directions around you.";
+            Rooms[1, 1].Description = "This is an open field west of a white house, with a boarded front door.";
+            Rooms[1, 2].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
+
+            Rooms[2, 0].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
+            Rooms[2, 1].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
+            Rooms[2, 2].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+        }
     }
 }
