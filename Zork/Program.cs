c73ab47cad12c861b494816a00f9603305ac0f8e
Deedo67 <70287389+Deedo67@users.noreply.github.com>
2021-10-06 19:01:17 -0400

    Zork 4.1 Commit
    Final commit for Zork 4.1

--- Zork/Program.cs
+++ Zork/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.IO;
 
 namespace Zork
 {
@@ -17,7 +18,9 @@ namespace Zork
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Zork!");
-            InitializeRoomDescriptions();
+
+            string roomsFilename = "Rooms.txt";
+            InitializeRoomDescriptions(roomsFilename);
 
             Room previousRoom = null;
             Commands command = Commands.UNKNOWN;
@@ -67,11 +70,11 @@ namespace Zork
             bool isValidMove = true;
             switch (command)
             {
-                case Commands.NORTH when Location.Row < 0:
+                case Commands.SOUTH when Location.Row < 0:
                     Location.Row--;
                     break;
 
-                case Commands.SOUTH when Location.Row > Rooms.GetLength(0) - 1:
+                case Commands.NORTH when Location.Row > Rooms.GetLength(0) - 1:
                     Location.Row++;
                     break;
 
@@ -100,24 +103,36 @@ namespace Zork
             { new Room ("Dense Woods"), new Room("North of House"), new Room("Clearing") }
         };
 
-        private static void InitializeRoomDescriptions()
+        private static readonly Dictionary<string, Room> RoomMap;
+
+        static Program()
         {
-            var roomMap = new Dictionary<string, Room>();
+            RoomMap = new Dictionary<string, Room>();
             foreach (Room room in Rooms)
             {
-                roomMap.Add(room.Name, room);
-                roomMap[room.Name] = room;
+                RoomMap[room.Name] = room;
             }
+        }
 
-            roomMap["Rocky Trail"].Description = "You are on a rock-strewn trail.";
-            roomMap["South of House"].Description = "You are facing the south side of a white house. There s no door here and all the windows are barred.";
-            roomMap["Canyon View"].Description = "You are at the top of the Great Conyon on its south wall.";
-            roomMap["Forest"].Description = "This is a forest, with trees in all directions around you.";
-            roomMap["West of House"].Description = "This is an open field west of a white house, with a boarded front door.";
-            roomMap["Behind House"].Description = "You are behind the white house. In one corner of the house there is a small window which is slightly ajar.";
-            roomMap["Dense Woods"].Description = "This is a dimly lit forest, with large trees all around. To the east, there appears to be sunlight.";
-            roomMap["North of House"].Description = "You are facing the north side of a white house. There is no door here, and all the windows are barred.";
-            roomMap["Clearing"].Description = "You are in a clearing, with a forest surrounding you on the west and south.";
+        private static void InitializeRoomDescriptions(string roomsFilename)
+        {
+            const string fieldDelimiter = "##";
+            const int expectedFieldCount = 2;
+
+            string[] lines = File.ReadAllLines(roomsFilename);
+            foreach (string line in lines)
+            {
+                string[] fields = line.Split(fieldDelimiter);
+                if (fields.Length != expectedFieldCount)
+                {
+                    throw new InvalidDataException("Invalid record.");
+                }
+
+                string name = fields[(int)Fields.Name];
+                string description = fields[(int)Fields.Description];
+
+                RoomMap[name].Description = description;
+            }
         }
 
         private static readonly Commands[] Directions =
@@ -129,5 +144,11 @@ namespace Zork
         };
 
         private static (int Row, int Column) Location = (1, 1);
+
+        private enum Fields
+        {
+            Name = 0,
+            Description
+        }
     }
 }
